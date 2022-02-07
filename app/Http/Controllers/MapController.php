c18fc76fabad9d686d3efd1cf566732d73aab8fb
Oscar Martinez <ojmartinezma@unal.edu.co>
2022-02-07 13:39:34 -0500

    Se agregan modelos y se mejora el api de mapa

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Estate;
+
+class MapController extends Controller
+{
+    //
+    public function api(){
+        //
+        $marker=Estate::select('id','latitude','longitude')->get();
+        $marker=Estate::select()->with('user')->with('feature')->first();
+        return json_encode($marker);
+    }
+
+    public function filter(){
+        //
+        $marker=Estate::select('id','latitude','longitude')->get();
+        return json_encode($marker);
+    }
+
+    public function detail($id){
+        //
+        $marker=Estate::where('id',$id)->with('user')->with('feature')->get();
+        return json_encode($marker);
+    }
+}
