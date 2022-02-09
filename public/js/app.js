8b2c1b60e19d33169e447dd6feea1f5471100670
Oscar Martinez <ojmartinezma@unal.edu.co>
2022-02-09 15:53:19 -0500

    corrección de errores de js de mapa

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ let markerClust;
 //console.log(markersApp)
 //console.log(JSON.parse('[{"id":1,"latitude":4.593636989593506,"longitude":-74.10095977783203},{"id":2,"latitude":4.6625629833684314,"longitude":-74.05248017561198}] '))
 //console.log(JSON.parse('{"id":1,"user_id":1,"value":500000,"type":"apartamento","area":40,"latitude":4.593636989593506,"longitude":-74.10095977783203,"city":"Bogot\u00e1","loc":"","neighborhood":"La frag\u00fcita","address":"carrera 25 # 7 - 18","estr":3,"area_h":40,"area_c":40,"rooms":3,"toilets":1,"floors":1,"parking":0,"created_at":"2022-02-04T04:30:27.000000Z","updated_at":"2022-02-06T13:27:01.000000Z","deleted_at":null,"user":{"id":1,"name":"Admin","email":"admin@casapp.test","email_verified_at":null,"created_at":"2021-12-15T01:24:26.000000Z","updated_at":"2021-12-15T01:24:26.000000Z","profile_photo_url":"https:\/\/ui-avatars.com\/api\/?name=Admin&color=7F9CF5&background=EBF4FF"},"feature":{"id":1,"estate_id":1,"furnished":0,"basement":1,"terrace":0,"security":0,"created_at":"2022-02-07T12:18:25.000000Z","update_at":"2022-02-07 12:18:25","deleted_at":null}}'))
-document.getElementById("btn-test-1").addEventListener("click",removeMarkers)
+//document.getElementById("btn-test-1").addEventListener("click",removeMarkers)
 
 //console.log(markersApp)
 //console.log(jsonToMarkers('[{"id":1,"latitude":4.593636989593506,"longitude":-74.10095977783203},{"id":2,"latitude":4.6625629833684314,"longitude":-74.05248017561198}] '))
